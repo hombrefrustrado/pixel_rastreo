2620eefa5829ec4253e74fe97c491115363b0d77
hombefrustrado <hombrefrustrado8@gmail.com>
2026-07-28 00:31:09 +0200

    primero

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, send_file, request 
+import logging
+app = Flask(__name__)
+
+@app.route("/pixel")
+def pixel():
+    ip = request.headers.get("X-Forwarded-For", request.remote_addr)
+    if ip:
+        ip = ip.split(",")[0].strip()
+
+    user_agent = request.headers.get("User-Agent", "Desconocido")
+
+    app.logger.info(f"IP: {ip} | User-Agent: {user_agent}")
+
+    return send_file(
+        "battie.jpg",
+        mimetype="image/jpg",
+        max_age=0
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
